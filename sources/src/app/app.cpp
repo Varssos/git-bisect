bc71d08f0375dfce9e35f4b5c711e4080894db56
Sebastian Wach <sebastianwach22@gmail.com>
2023-04-12 21:36:05 +0200

    Removed header

--- sources/src/app/app.cpp
+++ sources/src/app/app.cpp
@@ -1,7 +1,7 @@
 // #include "app/app.hpp"
 // #include "config/config.hpp"
 // #include "device/device.hpp"
-#include <iostream>
+// #include <iostream>
 
 
 int main()
@@ -10,7 +10,7 @@ int main()
     std::cout << "It works2" << std::endl;
     std::cout << "It works3" << std::endl;
     std::cout << "It works4" << std::endl;
-    
+
 
     return 0;
 }
